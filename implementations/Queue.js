9b31f631961c9056aad2535ad2f4efe8f6e7acd7
Pruthvi P <pruthvip15@gmail.com>
2017-04-10 20:47:57 +0530

    Feature - Add queue data struture

--- implementations/Queue.js
+++ implementations/Queue.js
@@ -3,15 +3,25 @@
  * @constructor
  */
 function Queue() {
-   
+   this._storage = [];
 }
 
 /**
  * Methods
  * @param value
  */
-Queue.prototype.method = function (value) {
-    
+Queue.prototype.enqueue = function (value) {
+  this._storage.push(value);
+};
+
+/**
+ * Methods
+ * @param value
+ */
+Queue.prototype.dequeue = function () {
+  const dequeuedElement = this._storage.splice(this._storage.length - 1, 1);
+
+  return dequeuedElement;
 };
 
 
--- test/Queue.test.js
+++ test/Queue.test.js
@@ -2,7 +2,21 @@ import assert from 'assert';
 import Queue from '../implementations/Queue';
 
 describe('Queue', function() {
-    it('Asserts', function() {
-       
+  const Q1 = new Queue();
+  const noOfElements = 5;
+  Q1.enqueue(31);
+  Q1.enqueue(41);
+  Q1.enqueue(51);
+  Q1.enqueue(61);
+  Q1.enqueue(71);
+
+  describe('FIFO Operations', function() {
+    it('first in first out property should be followed', function() {
+      assert.equal(71, Q1.dequeue());
+      assert.equal(61, Q1.dequeue());
+      assert.equal(51, Q1.dequeue());
+      assert.equal(41, Q1.dequeue());
+      assert.equal(31, Q1.dequeue());
     });
+  });
 });
